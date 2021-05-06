90b754ec0e5df31af2453f0e16c1679e57e72757
Piyush Goyani <thesourcepedia@gmail.com>
2021-05-06 11:01:24 +0530

    both micro-frontend application registered and root config updated for render with basic route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">DataTable Home</router-link> |
+    <router-link to="/about">DataTable About</router-link>
   </div>
   <router-view/>
 </template>
